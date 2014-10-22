f14db348004d1e9b728396a295b30c5251384a68
Carrick Rogers <carrickr@stanford.edu>
2014-10-22 09:21:43 -0700

    Updating the deploy environments.

--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -2,4 +2,4 @@ set :deploy_host, ask("Server", 'enter in the server you are deploying to. do no
 server "#{fetch(:deploy_host)}.stanford.edu", user: fetch(:user), roles: %w{web db app}
 
 Capistrano::OneTimeKey.generate_one_time_key!
-set :rails_env, "production"
+set :rails_env, "stage"
--- config/deploy/testing.rb
+++
@@ -1,4 +0,0 @@
-server 'lyberservices-test.stanford.edu', user: 'lyberadmin', roles: %w{web db app}
-
-Capistrano::OneTimeKey.generate_one_time_key!
-set :rails_env, "test"
