90f734bca227162bbe2832be7b4754a114e1dfc9
Carrick Rogers <carrickr@stanford.edu>
2015-05-19 14:31:28 -0700

    It is working init setup.

---
+++ config/initializers/is_it_working.rb
@@ -0,0 +1,10 @@
+require 'uri'
+require 'nokogiri'
+require 'is_it_working'
+Rails.configuration.middleware.use(IsItWorking::Handler) do |h|
+  # Check that the PURL NFS mount directory
+  h.check :directory, :path => "/purl", :permission => [:read]
+  
+  #Check that Solr is Working
+  h.check :rsolr, :client => IndexerController.establish_solr_connection
+end
