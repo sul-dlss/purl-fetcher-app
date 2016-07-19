67e359f166a2ed456579559ef4d787569a30f2fa
Darren Hardy <drh@stanford.edu>
2016-07-19 13:35:26 -0700

    relocate server declaration to config/deploy.rb with standard suffix

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,4 +1,3 @@
-server 'purl-fetcher-dev.stanford.edu', user: fetch(:user), roles: %w{web db app}
 set :bundle_without, %w(test deployment development).join(' ')
 
 Capistrano::OneTimeKey.generate_one_time_key!
