037101d5398360a386def10a6dece9dd27526761
Jack Reed <phillipjreed@gmail.com>
2016-08-16 17:47:14 -0400

    remove xml response views from app

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -14,7 +14,6 @@ class AboutController < ApplicationController
 
     respond_to do |format|
       format.json { render json: @result.to_json }
-      format.xml  { render json: @result.to_xml(root: 'status') }
       format.html { render }
     end
   end
