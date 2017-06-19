32cb7860044de9bcf362310fd0bb227cf915a238
Justin Coyne <jcoyne@justincoyne.com>
2017-06-19 14:26:30 -0500

    Update capistrano to department standards

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,3 +1,5 @@
+server "purl-fetcher-dev.stanford.edu", user: 'lyberadmin', roles: %w{web db app}
+
 set :bundle_without, %w(test deployment development).join(' ')
 
 Capistrano::OneTimeKey.generate_one_time_key!
