5028b82a31e2c21dba2fb6a879e1ab72b94f6542
Chris Beer <cabeer@stanford.edu>
2018-02-23 08:38:49 -0800

    Add ContentSearch as an always-true target for sending notifications

--- spec/features/docs_controller_spec.rb
+++ spec/features/docs_controller_spec.rb
@@ -19,11 +19,11 @@ describe(V1::DocsController, type: :request, integration: true) do
     results = JSON.parse(response.body)
     expected_results = { changes:
       [
-        { druid: "druid:dd111ee2222", latest_change: "2014-01-01T00:00:00Z", true_targets: ["SearchWorksPreview"], collections: ["druid:ff111gg2222"] },
-        { druid: "druid:bb111cc2222", latest_change: "2015-01-01T00:00:00Z", true_targets: ["SearchWorks", "Revs", "SearchWorksPreview"], collections: ["druid:ff111gg2222"] },
-        { druid: "druid:aa111bb2222", latest_change: "2016-06-06T00:00:00Z", true_targets: ["SearchWorksPreview"], collections: ["druid:gg111hh2222"] },
-        { druid: "druid:gg111hh2222", latest_change: "2016-06-08T00:00:00Z", true_targets: ["SearchWorksPreview"] },
-        { druid: "druid:hh111ii2222", latest_change: "2016-06-09T00:00:00Z", true_targets: ["SearchWorksPreview"] }
+        { druid: "druid:dd111ee2222", latest_change: "2014-01-01T00:00:00Z", true_targets: ["SearchWorksPreview", "ContentSearch"], collections: ["druid:ff111gg2222"] },
+        { druid: "druid:bb111cc2222", latest_change: "2015-01-01T00:00:00Z", true_targets: ["SearchWorks", "Revs", "SearchWorksPreview", "ContentSearch"], collections: ["druid:ff111gg2222"] },
+        { druid: "druid:aa111bb2222", latest_change: "2016-06-06T00:00:00Z", true_targets: ["SearchWorksPreview", "ContentSearch"], collections: ["druid:gg111hh2222"] },
+        { druid: "druid:gg111hh2222", latest_change: "2016-06-08T00:00:00Z", true_targets: ["SearchWorksPreview", "ContentSearch"] },
+        { druid: "druid:hh111ii2222", latest_change: "2016-06-09T00:00:00Z", true_targets: ["SearchWorksPreview", "ContentSearch"] }
       ],
       pages: pagination_response
     }
@@ -36,7 +36,7 @@ describe(V1::DocsController, type: :request, integration: true) do
     results = JSON.parse(response.body)
     expected_results = { changes:
       [
-        { druid: "druid:dd111ee2222", latest_change: "2014-01-01T00:00:00Z", true_targets: ["SearchWorksPreview"], collections: ["druid:ff111gg2222"] }
+        { druid: "druid:dd111ee2222", latest_change: "2014-01-01T00:00:00Z", true_targets: ["SearchWorksPreview", "ContentSearch"], collections: ["druid:ff111gg2222"] }
       ], pages: pagination_response
     }
     expect(results).to eq(expected_results.with_indifferent_access)
