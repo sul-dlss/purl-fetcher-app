6408ae8a1fda535fde497b83e9187d05d5afcfdd
Chris Beer <cabeer@stanford.edu>
2018-06-15 09:43:02 -0700

    Remove PurlListener infrastructure

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -16,38 +16,3 @@ class VersionCheck < OkComputer::AppVersionCheck
   end
 end
 OkComputer::Registry.register 'version', VersionCheck.new
-
-# Check to see if process is running
-class PidCheck < OkComputer::Check
-  def initialize(pid)
-    @pid = pid
-  end
-
-  def check
-    pid = if @pid.respond_to?(:call)
-            @pid.call
-          else
-            @pid
-          end
-    if pid.present?
-      begin
-        if Process.kill(0, pid)
-          mark_message "process #{pid} is running"
-        else
-          mark_message "process #{pid} is not responding"
-          mark_failure
-        end
-      rescue Errno::ESRCH, Errno::EPERM
-        mark_message "process #{pid} is not running properly"
-        mark_failure
-      end
-    else
-      mark_message "no listener process to check?"
-      mark_failure
-    end
-  end
-end
-
-# We don't know the pid for the listener until the check method is called
-getpid = proc { ListenerLog.current.present? ? ListenerLog.current.process_id : nil }
-OkComputer::Registry.register 'feature-listener-process', PidCheck.new(getpid)
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,16 +1,3 @@
 # Learn more: http://github.com/javan/whenever
 
 set :output, '/dev/null'
-
-every 15.minutes do
- rake 'find:deletes[15]' # scan .deletes directory for new deletes
-end
-
-# ensures that all changes touch files are processed if the listener was down
-every 1.hour do
-  rake 'listener:recent_changes:process'
-end
-
-every :reboot do
-  rake 'listener:restart'
-end
