93164c621801d2bced425815d37577f0c4a95a1a
Chris Beer <cabeer@stanford.edu>
2019-05-29 07:50:15 -0700

    Update the deleted endpoint to filter by the updated_at range instead of using potentially stale data from the purl xml itself

--- app/controllers/v1/docs_controller.rb
+++ app/controllers/v1/docs_controller.rb
@@ -19,7 +19,8 @@ module V1
 
     # API call to get a full list of all purl deletes between two times
     def deletes
-      @deletes = Purl.where(deleted_at: @first_modified..@last_modified)
+      @deletes = Purl.where(updated_at: @first_modified..@last_modified)
+                     .where.not(deleted_at: nil)
                      .target('target' => params[:target])
                      .reorder(:deleted_at) # used to override the default_scope
                      .page(page_params[:page])
